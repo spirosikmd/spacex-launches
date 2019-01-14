7deb16a9fbcfdffe2f98e38c012a500389c7791c
Spyros Ioakeimidis <spyrosikmd@gmail.com>
2019-01-14 20:23:59 +0100

    Remove link from logo

--- src/TopBar.js
+++ src/TopBar.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from '@reach/router';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
@@ -11,24 +10,16 @@ const styles = theme => ({
     marginLeft: -theme.spacing.unit,
     marginRight: theme.spacing.unit,
   },
-  link: {
-    textDecoration: 'none',
-    color: theme.palette.common.white,
-    display: 'flex',
-    alignItems: 'center',
-  },
 });
 
 const TopBar = ({ classes }) => {
   return (
     <AppBar position="static">
       <Toolbar>
-        <Link to="/" className={classes.link}>
-          <GroupWorkRoundedIcon className={classes.logo} />
-          <Typography variant="h6" color="inherit">
-            SpaceX Launches
-          </Typography>
-        </Link>
+        <GroupWorkRoundedIcon className={classes.logo} />
+        <Typography variant="h6" color="inherit">
+          SpaceX Launches
+        </Typography>
       </Toolbar>
     </AppBar>
   );
